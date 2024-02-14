89661330afef3486d78a5e3628c0f6cb2c3d5758
Sudip Shrestha <71699219+sudipsudip001@users.noreply.github.com>
2024-02-14 20:44:18 +0545

    Update refinedShare.py
    still needs work

--- refinedShare.py
+++ refinedShare.py
@@ -2,14 +2,15 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from pathlib import Path
 import time
 
-dic = {}
+info = {}
 
 print('Enter the number of Kitta to apply.')
 number = input()
-dic['number'] = number
+info['number'] = number
 file = open('data.txt')
 for line in file:
     x = line.split('=\'')
@@ -17,54 +18,78 @@ for line in file:
     b = x[1]
     c = len(b)-2
     b = b[0:c]
-    dic[a] = b
+    info[a] = b
 
 driver = webdriver.Firefox()
 driver.get('https://meroshare.cdsc.com.np/#/login')
 time.sleep(2)
 
-branch = driver.find_element_by_id('selectBranch')
+branch = driver.find_element(By.ID, 'selectBranch')
 branch.click()
-branchId = driver.find_element_by_class_name('select2-search__field')
+branchId = driver.find_element(By.CLASS_NAME, 'select2-search__field')
 branchId.click()
-branchId.send_keys(dic['dpid'])
+branchId.send_keys(info['dpid'])
 branchId.send_keys(Keys.ENTER)
 
-user = driver.find_element_by_id('username')
-user.send_keys(dic['username'])
+user = driver.find_element(By.ID, 'username')
+user.send_keys(info['username'])
 
-passer = driver.find_element_by_id('password')
-passer.send_keys(dic['password'])
+passer = driver.find_element(By.ID, 'password')
+passer.send_keys(info['password'])
 
 time.sleep(2)
 passer.submit()
 
 time.sleep(3)
 # portion to select the myASBA section
-link = driver.find_element_by_link_text('My ASBA')
+link = driver.find_element(By.LINK_TEXT, 'My ASBA')
 link.click()
 
 time.sleep(1)
+
+# improved version or should I say, 'the corrected' version
+
 # to find if there's an IPO section with ordinary shares
-try:
-    clicker = driver.find_element_by_class_name('btn-issue')
-    clicker.click()
-    bank = driver.find_element_by_xpath('//*[@id="selectBank"]')
-    bank.click()
-    bank.send_keys(Keys.DOWN)
-    bank.send_keys(Keys.ENTER)
-    bank.click()
-    bank.send_keys(Keys.DOWN)
-    bank.send_keys(Keys.ENTER)
-    num = driver.find_element_by_id('appliedKitta')
-    num.send_keys(dic['number'])
-    crn = driver.find_element_by_id('crnNumber')
-    crn.send_keys(dic['crn'])
-    dis = driver.find_element_by_id('disclaimer')
-    dis.click()
-    link = driver.find_element_by_xpath('/html/body/app-dashboard/div/main/div/app-issue/div/wizard/div/wizard-step[1]/form/div[2]/div/div[5]/div[2]/div/button[1]').click()
-    trans = driver.find_element_by_id('transactionPIN')
-    trans.send_keys(dic['pin'])
-    final = driver.find_element_by_xpath('/html/body/app-dashboard/div/main/div/app-issue/div/wizard/div/wizard-step[2]/div[2]/div/form/div[2]/div/div/div/button[1]').click()
-except:
-    print('No Ordinary shares open at the moment.')
+# first we'll try and create iterations for the selection of divs such that only the correct ones are selected by the users.
+reportSelector = driver.find_element(By.LINK_TEXT, 'Apply for Issue')
+reportSelector.click()
+time.sleep(3)
+main_divs = driver.find_elements(By.CSS_SELECTOR, '.company-list')
+
+target_text = 'Ordinary Shares'
+
+for main_div in main_divs:
+    span_elements = main_div.find_elements(By.CSS_SELECTOR, ".isin")
+    for i, span_element in enumerate(span_elements):
+        current_text = span_element.text
+
+        # Compare the text content
+        if current_text == target_text:
+            print(f"Span element {i + 1} text matches the target text: {target_text}")
+            # fill the share and exit
+            try:
+                clicker = driver.find_element(By.CLASS_NAME, 'btn-issue')
+                clicker.click()
+                bank = driver.find_element(By.XPATH, '//*[@id="selectBank"]')
+                bank.click()
+                bank.send_keys(Keys.DOWN)
+                bank.send_keys(Keys.ENTER)
+                bank.click()
+                bank.send_keys(Keys.DOWN)
+                bank.send_keys(Keys.ENTER)
+                num = driver.find_element(By.ID, 'appliedKitta')
+                num.send_keys(info['number'])
+                crn = driver.find_element(By.ID, 'crnNumber')
+                crn.send_keys(info['crn'])
+                dis = driver.find_element(By.ID, 'disclaimer')
+                dis.click()
+                link = driver.find_element(By.XPATH, '/html/body/app-dashboard/div/main/div/app-issue/div/wizard/div/wizard-step[1]/form/div[2]/div/div[5]/div[2]/div/button[1]').click()
+                trans = driver.find_element(By.ID, 'transactionPIN')
+                trans.send_keys(info['pin'])
+                final = driver.find_element(By.XPATH, '/html/body/app-dashboard/div/main/div/app-issue/div/wizard/div/wizard-step[2]/div[2]/div/form/div[2]/div/div/div/button[1]').click()
+                exit()
+            except:
+                print('No Ordinary shares open at the moment.')
+        else:
+            print(f"Span element {i + 1} text does not match the target text: {target_text}")
+driver.quit()
